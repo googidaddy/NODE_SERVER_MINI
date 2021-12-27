3f9d09d7f1630ed4f8aaa9b4b94b696c204cda3f
DengYao <dlvjc1998@gmail.com>
2021-12-27 10:21:04 +0800

    ss

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import dotenv from 'dotenv'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import path from 'path'
 dotenv.config( { path: path.join( __dirname, '..', '.env' ) } )
 
 const main = () => {
   const app = express()
   const PORT = process.env.PORT
+  app.use( express.static( path.join( __dirname, '..', 'public' ) ) )
+  app.use( express.json() )
+  app.use( express.urlencoded( { extended: true } ) )
+
+  app.get( '/', ( req: Request, res: Response ) => {
+    res.send( 'Hello World!' )
+  } )
+
   app.listen( PORT, () => {
     console.log( `Server on http://localhost:${PORT}` )
   } )
